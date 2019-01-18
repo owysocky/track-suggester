ee13529edf1e35c5139e94bd9a67966cad053d97
ow <volovykolya@gmail.com>
2019-01-18 09:14:58 -0800

    add toggle to the introduction texts

---
+++ js/scripts.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+
+  $(".cSharp").click(function(){
+    $(".cSharp h2").toggle();
+    $(".cSharp p").toggle();
+  });
+  $(".java").click(function(){
+    $(".java h2").toggle();
+    $(".java p").toggle();
+  });
+  $(".php").click(function(){
+    $(".php h2").toggle();
+    $(".php p").toggle();
+  });
+  $(".rubby").click(function(){
+    $(".rubby h2").toggle();
+    $(".rubby p").toggle();
+  });
+  $(".css").click(function(){
+    $(".css h2").toggle();
+    $(".css p").toggle();
+  });
+
+
+
+
+
+
+
+
+
+});
